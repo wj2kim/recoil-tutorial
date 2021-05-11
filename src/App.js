cb373d3e8a891301a4a4180c5b1d6acb92c26841
김우정 <wjkim@rsupport.com>
2021-05-11 15:27:55 +0900

    recoil tutorial environment setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
+import { atom, useRecoilState } from "recoil";
+
+const reposState = atom({
+  key: "repos",
+  default: [],
+})
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [repos, setRepos] = useRecoilState(reposState);
+
+  useEffect(() => {
+    const getRepos = async() => {
+      const url = "https://ghapi.huchen.dev/repositories?since=monthly";
+      const response = await fetch(url);
+      const body = await response.json();
+      console.log(body);
+    };
+
+    getRepos();
+  }, []);
+
+  return <div></div>;
 }
 
 export default App;
